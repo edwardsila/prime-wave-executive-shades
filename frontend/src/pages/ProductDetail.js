3d7737caec1276b1340a64a5232dc9edb0641be3
edwardsila <edwardsila000@gmail.com>
2026-04-16 21:20:04 +0300

    Fix ESLint - remove unused useCallback import

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import Logo from '../components/Logo';
